687075697ab91c6fb17995f90eb2af4c4f7450ac
Chris Bynum <cbynum@cdbcap.com>
2015-08-20 16:01:49 -0600

    convert data types before storing in mongo

--- collect-data/collect_books.py
+++ collect-data/collect_books.py
@@ -8,15 +8,24 @@ symbol = 'ltcusd'
 limit = 25
 book_url = '{0}/book/{1}?limit_bids={2}&limit_asks={2}'.format(api,
                                                                symbol, limit)
-
 client = MongoClient()
 db = client['bitmicro']
 ltc_books = db['ltc_books']
 
 
+def format_book_entry(entry):
+    if all(key in entry for key in ('amount', 'price', 'timestamp')):
+        entry['amount'] = float(entry['amount'])
+        entry['price'] = float(entry['price'])
+        entry['timestamp'] = float(entry['timestamp'])
+
+    return entry
+
+
 def get_json(url):
     resp = urllib2.urlopen(url)
-    return json.load(resp), resp.getcode()
+    return json.load(resp, object_hook=format_book_entry), resp.getcode()
+
 
 while True:
     start = time.time()
--- collect-data/collect_trades.py
+++ collect-data/collect_trades.py
@@ -5,18 +5,29 @@ from pymongo import MongoClient
 
 api = 'https://api.bitfinex.com/v1'
 symbol = 'ltcusd'
-limit = 10000
+limit = 1000
 
 client = MongoClient()
 db = client['bitmicro']
 ltc_trades = db['ltc_trades']
 
 
+def format_trade(trade):
+    if all(key in trade for key in ('tid', 'amount', 'price', 'timestamp')):
+        trade['_id'] = trade.pop('tid')
+        trade['amount'] = float(trade['amount'])
+        trade['price'] = float(trade['price'])
+        trade['timestamp'] = float(trade['timestamp'])
+
+    return trade
+
+
 def get_json(url):
     resp = urllib2.urlopen(url)
-    return json.load(resp), resp.getcode()
+    return json.load(resp, object_hook=format_trade), resp.getcode()
 
-last = 1
+
+last = 0
 while True:
     url = '{0}/trades/{1}?timestamp={2}&limit_trades={3}'.format(api, symbol,
                                                                  last, limit)
@@ -26,7 +37,6 @@ while True:
         print code
     else:
         for trade in trades:
-            trade['_id'] = trade.pop('tid')
             ltc_trades.update_one({'_id': trade['_id']},
                                   {'$setOnInsert': trade}, upsert=True)
 
