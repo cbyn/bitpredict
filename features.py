fbdddb419540508e4c2c451144baf68096598a90
Ubuntu <ubuntu@ip-172-31-42-253.eu-west-1.compute.internal>
2015-09-05 21:05:59 +0000

    add aggressor measure

--- features.py
+++ features.py
@@ -10,7 +10,6 @@ db = client['bitmicro']
 
 # TODO:
 # volume on best orders as a direct feature
-# trades buy/sell ratio
 # trades total volume
 # book total volume
 # time-weight trades?
@@ -82,7 +81,7 @@ def get_imbalance(books):
             return x.amount*(.5*book.width/(x.price-book.mid))**2
         bid_imbalance = book.bids.apply(calc, axis=1)
         ask_imbalance = book.asks.apply(calc, axis=1)
-        return bid_imbalance.sum()/ask_imbalance.sum()
+        return bid_imbalance.sum() - ask_imbalance.sum()
         # return (bid_imbalance-ask_imbalance).sum()
     books = books.apply(calc_imbalance, axis=1)
     print 'get_imbalance run time:', (time()-start)/60, 'minutes'
@@ -105,6 +104,16 @@ def get_trade_df(symbol, min_ts, max_ts, convert_timestamps=False):
     return trades
 
 
+def get_trades_in_range(trades, ts, offset):
+    '''
+    Returns trades in a given timestamp range
+    '''
+    ts = int(ts)
+    i_0 = trades.timestamp.searchsorted([ts-offset], side='left')[0]
+    i_n = trades.timestamp.searchsorted([ts-1], side='right')[0]
+    return trades.iloc[i_0:i_n]
+
+
 def get_trades_average(books, trades, offset):
     '''
     Returns a volume-weighted average of trades for each data point in
@@ -113,10 +122,7 @@ def get_trades_average(books, trades, offset):
     start = time()
 
     def mean_trades(ts):
-        ts = int(ts)
-        i_0 = trades.timestamp.searchsorted([ts-offset], side='left')[0]
-        i_n = trades.timestamp.searchsorted([ts-1], side='right')[0]
-        trades_n = trades.iloc[i_0:i_n]
+        trades_n = get_trades_in_range(trades, ts, offset)
         if not trades_n.empty:
             return (trades_n.price*trades_n.amount/trades_n.amount).sum()
         # return trades.iloc[i_0:i_n].price.mean()
@@ -124,15 +130,26 @@ def get_trades_average(books, trades, offset):
     return books.index.map(mean_trades)
 
 
-def get_aggressors(books, trades, offset):
+def get_aggressor(books, trades, offset):
     '''
-    Return
+    Returns a measure of whether trade aggressors were buyers or sellers for
+    each data point in DataFrame of book data
     '''
+    start = time()
+
+    def aggressor(ts):
+        trades_n = get_trades_in_range(trades, ts, offset)
+        buys = trades_n['type'] == 'buy'
+        buy_vol = trades_n[buys].amount.sum()
+        sell_vol = trades_n[~buys].amount.sum()
+        return buy_vol - sell_vol
+    print 'get_aggressor run time:', (time()-start)/60, 'minutes'
+    return books.index.map(aggressor)
 
 
 def check_times(books):
     '''
-    Returns list of differeces between collection time and max book timestamps
+    Returns list of differences between collection time and max book timestamps
     '''
     time_diff = []
     for i in range(len(books)):
@@ -146,7 +163,7 @@ def check_times(books):
 
 def make_features(symbol, sample, mid_offsets, trades_offsets):
     '''
-    Returns a dataframe with mid targets and features
+    Returns a DataFrame with mid targets and features
     '''
     start = time()
     books = get_book_df(symbol, sample)
@@ -159,19 +176,19 @@ def make_features(symbol, sample, mid_offsets, trades_offsets):
     books['imbalance'] = get_imbalance(books)
     min_ts = books.index[0] - trades_offsets[-1]
     max_ts = books.index[-1]
-    ltc_trades = get_trade_df(symbol, min_ts, max_ts)
+    trades = get_trade_df(symbol, min_ts, max_ts)
     # Fill trade NaNs with zero (there are no trades in range)
     for n in trades_offsets:
-        books['trades{}'.format(n)] = \
-            get_trades_average(books, ltc_trades, n)
+        books['trades{}'.format(n)] = get_trades_average(books, trades, n)
         books['trades{}'.format(n)] = \
             (books.mid / books['trades{}'.format(n)]).apply(log).fillna(0)
+        books['aggressor{}'.format(n)] = get_aggressor(books, trades, n)
     print 'make_features run time:', (time()-start)/60, 'minutes'
     # Drop observations where y is NaN
     return books.drop(['bids', 'asks'], axis=1).dropna()
 
 
-def fit_model(X, y, chunk):
+def fit_classifier(X, y, chunk):
     '''
     Fits a model to data
     '''
@@ -187,6 +204,24 @@ def fit_model(X, y, chunk):
                                    max_depth=10,
                                    random_state=42,
                                    n_jobs=-1)
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    return model, score
+
+
+def fit_regressor(X, y, chunk):
+    '''
+    Fits a model to data
+    '''
+    from sklearn.ensemble import RandomForestRegressor
+    X_train, X_test = train_test_split(X, chunk)
+    y_train, y_test = train_test_split(y, chunk)
+
+    model = RandomForestRegressor(n_estimators=100,
+                                  min_samples_leaf=500,
+                                  max_depth=10,
+                                  random_state=42,
+                                  n_jobs=-1)
     model.fit(X_train, y_train)
     score = model.score(X_test, y_test)
     return model, score
@@ -208,25 +243,33 @@ def train_test_split(data, chunk):
 
 def run_models(data, chunk):
     '''
-    Runs a grid search on mid and trades offsets
+    Runs model with a range of target offsets
     '''
     mids = [col for col in data.columns if 'mid' in col]
     trades = [col for col in data.columns if 'trades' in col]
-    scores = {}
+    aggressors = [col for col in data.columns if 'aggressor' in col]
+    classifier_scores = {}
+    regressor_scores = {}
     for m in mids:
         y = data[m].values
-        X = data[['width', 'imbalance']+trades].values
-        _, score = fit_model(X, y, chunk)
-        scores[score] = m
-    for score in sorted(scores):
-        print scores[score], score
+        X = data[['width', 'imbalance']+trades+aggressors].values
+        _, classifier_score = fit_classifier(X, y, chunk)
+        classifier_scores[classifier_score] = m
+        _, regressor_score = fit_regressor(X, y, chunk)
+        regressor_scores[regressor_score] = m
+    print 'classifier accuracies:'
+    for score in sorted(classifier_scores):
+        print classifier_scores[score], score
+    print 'regressor r^2:'
+    for score in sorted(regressor_scores):
+        print regressor_scores[score], score
 
 
 def make_data(symbol, sample):
     data = make_features(symbol,
                          sample=sample,
-                         mid_offsets=[5, 10, 30, 60, 120, 300, 600],
-                         trades_offsets=[5, 10, 30, 60, 120, 300, 600])
+                         mid_offsets=[5, 10, 30, 60],
+                         trades_offsets=[30, 60, 120, 300, 600])
     return data
 
 if __name__ == '__main__' and len(sys.argv) == 4:
