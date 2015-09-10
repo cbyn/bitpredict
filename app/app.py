852c2160d80eb5ca83efc2d30c7b117db0704b34
Ubuntu <ubuntu@ip-172-31-23-39.eu-west-1.compute.internal>
2015-09-10 23:30:40 +0000

    first attempt at live predictions

--- app/app.py
+++ app/app.py
@@ -2,14 +2,18 @@ from bitmicro.model import features as f
 import pymongo
 import time
 import sys
+import pickle
 
 client = pymongo.MongoClient()
 db = client['bitmicro']
 symbol = sys.argv[1]
 duration = int(sys.argv[2])
 predictions = db[symbol+'_predictions']
+with open('cols.pkl', 'r') as file1:
+    cols = pickle.load(file1)
+with open('model.pkl', 'r') as file2:
+    model = pickle.load(file2)
 
-# Need to import a pickled model
 while True:
     start = time.time()
     data = f.make_features(symbol,
@@ -19,10 +23,10 @@ while True:
                            [0, 2, 4, 8],
                            True)
     current_price = data.pop('mid').iloc[0]
-    pred = 0
+    pred = model.predict(data[cols].values)[0]
     entry = {'prediction': pred,
              'current_price': current_price,
-             'future_price': current_price}
+             'future_price': 0}
     predictions.update_one({'_id': data.index[0]},
                            {'$setOnInsert': entry},
                            upsert=True)
