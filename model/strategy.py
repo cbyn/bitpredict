3b9ce5e61bf8c045777d83694f0c2ea60c7bcaa2
Ubuntu <ubuntu@ip-172-31-42-253.eu-west-1.compute.internal>
2015-09-10 01:03:01 +0000

    run strategy assuming single trades paying full spread

--- model/strategy.py
+++ model/strategy.py
@@ -1,53 +1,48 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pickle
 
-with open('data2.pkl', 'r') as f:
+with open('../new_data.pkl', 'r') as f:
     data = pickle.load(f)
 
-cols = ['width',
-        'prev10',
-        'adjusted_price',
-        'imbalance',
-        'trades30',
-        'aggressor30',
-        'trend30',
-        'trades120',
-        'aggressor120',
-        'trend120']
+cols = [u'aggressor30', u'adjusted_price4', u'imbalance4', u'aggressor120',
+        u'trades300', u'aggressor10', u'trend120', u'trend30', u'trades10']
 
+data = data[data.width > 0]
 X = data[cols]
-y = data.mid10
-X_train1 = X.iloc[:400000]
-X_train2 = X.iloc[400000:800000]
-X_test = X.iloc[800000:]
-y_train1 = y.iloc[:400000]
-y_train2 = y.iloc[400000:800000]
-y_test = y.iloc[800000:]
-regressor = RandomForestRegressor(n_estimators=50,
+y = data.mid30
+X_train = X.iloc[:650000]
+X_test = X.iloc[650000:]
+y_train = y.iloc[:650000]
+y_test = y.iloc[650000:]
+widths = data.iloc[650000:].width/250
+regressor = RandomForestRegressor(n_estimators=100,
                                   min_samples_leaf=500,
                                   random_state=42,
                                   n_jobs=-1)
+regressor.fit(X_train.values, y_train.values)
+print 'r-squared', regressor.score(X_test.values, y_test.values)
+preds = regressor.predict(X_test)
+trades = np.zeros(len(preds))
+costs = np.zeros(len(preds))
+count = 0
+active = False
+for i, pred in enumerate(preds):
+    if active:
+        count += 1
+        if count == 30:
+            count = 0
+            active = False
+    elif abs(pred) > .0005:
+        active = True
+        trades[i] = np.sign(pred)
+        costs[i] = (widths.iloc[i] + widths.iloc[i+30])/2
 
-classifier = RandomForestClassifier(n_estimators=50,
-                                    min_samples_leaf=500,
-                                    random_state=42,
-                                    n_jobs=-1)
-
-classifier.fit(X_train1.values, np.sign(y_train1.apply(abs).values))
-movement1 = classifier.predict(X_train2)
-print 'accuracy', classifier.score(X_train2,
-                                   np.sign(y_train2.apply(abs).values))
-regressor.fit(X_train2[movement1 == 1].values, y_train2[movement1 == 1].values)
-movement2 = classifier.predict(X_test)
-print 'accuracy', classifier.score(X_test,
-                                   np.sign(y_test.apply(abs).values))
-print 'r-squared', regressor.score(X_test[movement2 == 1].values,
-                                   y_test[movement2 == 1].values)
-# signal = np.sign(preds)
-# returns = signal*y_test
-# profit = np.cumsum(returns)
-# plt.plot(profit)
-# plt.show()
+returns = trades*y_test - costs
+mean_return = returns[trades != 0].mean()
+mean_cost = data.width.mean()/250
+print 'average return', mean_return
+profit = np.cumsum(returns)
+plt.plot(profit)
+plt.show()
