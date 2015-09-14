ede43e8ba144f4da801c84553cd25245827d5816
Ubuntu <ubuntu@ip-172-31-42-147.eu-west-1.compute.internal>
2015-09-14 18:11:10 +0000

    clean up

--- app/check_preds.py
+++ app/check_preds.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import pymongo
-import itertools
 from sklearn.metrics import r2_score
 from math import log
 import sys
@@ -21,12 +20,3 @@ df['actual'] = (df.future_price/df.current_price).apply(log)
 score = r2_score(df.actual.values, df.prediction.values)
 print 'observations:', len(df)
 print 'r^2:', score
-
-
-def grouper(n, iterable):
-    it = iter(iterable)
-    while True:
-        chunk = tuple(itertools.islice(it, n))
-        if not chunk:
-            return
-        yield chunk
