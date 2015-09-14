dd8f8142bb867f75f28dd073dac15598b3b5df81
Ubuntu <ubuntu@ip-172-31-42-147.eu-west-1.compute.internal>
2015-09-14 17:00:38 +0000

    make limit configurable from command line

--- app/check_preds.py
+++ app/check_preds.py
@@ -3,12 +3,18 @@ import pymongo
 import itertools
 from sklearn.metrics import r2_score
 from math import log
+import sys
 
 client = pymongo.MongoClient()
 db = client['bitmicro']
 predictions = db['btc_predictions']
 
-cursor = predictions.find().limit(86400).sort('_id', pymongo.DESCENDING)
+if len(sys.argv) > 1:
+    limit = int(sys.argv[1])
+else:
+    limit = 86400
+
+cursor = predictions.find().limit(limit).sort('_id', pymongo.DESCENDING)
 df = pd.DataFrame(list(cursor))
 df = df[df.future_price != 0]
 df['actual'] = (df.future_price/df.current_price).apply(log)
