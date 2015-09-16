ea26d618d22fabbb33e1fa6f22e2f6ab0f2d14f1
Ubuntu <ubuntu@ip-172-31-42-147.eu-west-1.compute.internal>
2015-09-16 16:31:12 +0000

    initial app setup

--- app/run_charts.py
+++ app/run_charts.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import pymongo
-from bokeh.plotting import cursession, figure, show, output_server
+from bokeh.plotting import cursession, figure, output_server, push
+from bokeh import embed
+from json import load
+from urllib2 import urlopen
 import time
+import re
 
 client = pymongo.MongoClient()
 db = client['bitmicro']
@@ -16,11 +20,25 @@ y = data.prediction*100*100
 
 output_server('model_signal')
 p = figure(title='30-second Prediction',
+           plot_width=800,
+           plot_height=250,
            x_axis_type='datetime',
            x_axis_label='Greenwich Time',
-           y_axis_label='Basis Points')
+           y_axis_label='Basis Points',
+           tools='')
+p.toolbar_location = None
 p.line(x, y, name='signal')
-show(p)
+push()
+
+ip = load(urlopen('http://jsonip.com'))['ip']
+ssn = cursession()
+tag = embed.autoload_server(p, ssn, public=True).replace('localhost', ip)
+
+with open('templates/index.html', 'r') as f:
+    html = f.read()
+html = re.sub(r'<script.*?></script>', tag, html, 1, re.DOTALL)
+with open('templates/index.html', 'w+') as f:
+    f.write(html)
 
 renderer = p.select(dict(name='signal'))
 ds = renderer[0].data_source
@@ -32,5 +50,5 @@ while True:
     data = data.sort_index(ascending=True)
     ds.data['x'] = pd.to_datetime(data.index, unit='s').to_series()
     ds.data['y'] = data.prediction*100*100
-    cursession().store_objects(ds)
+    ssn.store_objects(ds)
     time.sleep(1)
