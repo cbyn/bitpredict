ac44a7242418d38a8451f756875347682e6eb043
Ubuntu <ubuntu@ip-172-31-42-147.eu-west-1.compute.internal>
2015-09-18 23:06:10 +0000

    initial working site

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/extended')
+def extended():
+    return render_template('extended.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
