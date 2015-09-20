027d4fb45a1bdf911d6b71b24c160c7dd76b74a2
Ubuntu <ubuntu@ip-172-31-42-147.eu-west-1.compute.internal>
2015-09-20 19:49:26 +0000

    final chart tweeks

--- app/app.py
+++ app/app.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/extended')
 def extended():
     return render_template('extended.html')
+
+
+@app.route('/performance')
+def performance():
+    return render_template('performance.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
